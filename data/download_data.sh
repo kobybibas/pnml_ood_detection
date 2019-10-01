781b6bd748edede375060cd0c6f797353859f846
Koby Bibas <kobybibas@gmail.com>
2019-10-01 16:17:48 +0300

    more files

---
+++ data/download_data.sh
@@ -0,0 +1,15 @@
+echo Imagenet
+wget https://www.dropbox.com/s/avgm2u562itwpkl/Imagenet.tar.gz
+tar -xvzf Imagenet.tar.gz
+echo Imagenet_resize
+wget https://www.dropbox.com/s/kp3my3412u5k9rl/Imagenet_resize.tar.gz
+tar -xvzf Imagenet_resize.tar.gz
+echo LSUN
+wget https://www.dropbox.com/s/fhtsw1m3qxlwj6h/LSUN.tar.gz
+tar -xvzf LSUN.tar.gz
+echo LSUN_resize
+wget https://www.dropbox.com/s/moqh2wh8696c3yl/LSUN_resize.tar.gz
+tar -xvzf LSUN_resize.tar.gz
+echo iSUN
+wget https://www.dropbox.com/s/ssz7qxfqae0cca5/iSUN.tar.gz
+tar -xvzf iSUN.tar.gz
